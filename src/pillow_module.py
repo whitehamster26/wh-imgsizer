a3a8251f3d28e1a18a9c9b32a669d8d5931141ae
DSemenko <whitehamster26@gmail.com>
2020-10-10 15:10:09 +0300

    Initial commit

---
+++ src/pillow_module.py
@@ -0,0 +1,2 @@
+def hello_world():
+    return print('Hello world!')
---
+++ src/scripts/run_app.py
@@ -0,0 +1,9 @@
+from src import pillow_module
+
+
+def main():
+    pillow_module.hello_world()
+
+
+if __name__ == "__main__":
+    main()
